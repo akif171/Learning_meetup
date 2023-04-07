5ad542c4482140f04a000982a52364a594119c34
akif171 <akifali229@gmail.com>
2023-04-07 17:02:05 +0500

    react learning meetup project

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMettupsPage() {
+  const [isLoading, setisLoading] = useState(true);
+  const [loadedMeetups, setloadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setisLoading(true);
+
+    fetch(
+      "https://react-meetup-project-390-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+        console.log(data);
+
+        setisLoading(false);
+        setloadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMettupsPage;
